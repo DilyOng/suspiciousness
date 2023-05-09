a2aedcaad096981d74c3f536081cc44b3c3722c6
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-05-09 14:59:57 +0100

    remove explicit alphas

--- suspiciousness/plot.py
+++ suspiciousness/plot.py
@@ -8,8 +8,8 @@ def sigma8plot(*nestedsamples, ax=None, x="omegam", y="sigma8", plot_kwargs):
     if ax is None:
         _, ax = plt.subplots()
 
-    for ns, alpha in zip(nestedsamples, (0.5, 0.5, 0.9)):
-        ns.plot.kde_2d(x, y, ax=ax, alpha=alpha, **plot_kwargs[ns.label])
+    for ns in nestedsamples:
+        ns.plot.kde_2d(x, y, ax=ax, **plot_kwargs[ns.label])
 
     ax.set(xlabel=nestedsamples[0].get_label(x),
            ylabel=nestedsamples[0].get_label(y),
@@ -23,9 +23,8 @@ def cornerplot(axes, *nestedsamples, prior=False, plot_kwargs):
     if prior:
         nestedsamples[0].set_beta(0).plot_2d(axes, alpha=0.25, color="blue", label="prior")
 
-    for ns, alpha in zip(nestedsamples,
-                                (0.5, 0.5, 0.9)):
-        ns.plot_2d(axes, alpha=alpha, **plot_kwargs[ns.label])
+    for ns in nestedsamples:
+        ns.plot_2d(axes, **plot_kwargs[ns.label])
     axes.iloc[1, 0].legend(loc=(3, 0.5))
 
     return axes

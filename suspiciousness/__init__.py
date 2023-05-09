a9d1739103d43b89b4b124880e40b59835f3b877
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-05-09 13:34:51 +0100

    add corner plot

--- suspiciousness/__init__.py
+++ suspiciousness/__init__.py
@@ -1,2 +1,2 @@
 from suspiciousness.core import logR, logI, logS, logp
-from suspiciousness.plot import plot
+from suspiciousness.plot import plot_sigma8, plot_corner
--- suspiciousness/plot.py
+++ suspiciousness/plot.py
@@ -4,14 +4,23 @@ from suspiciousness.core import samples
 
 
 @samples
-def plot(a, b, ab, labels=("a", "b", "ab"), ax=None, x="omegam", y="sigma8"):
+def plot_sigma8(a, b, ab, labels=("a", "b", "ab"), ax=None, x="omegam", y="sigma8"):
     if ax is None:
         _, ax = plt.subplots()
 
-    for ns, label in zip((a, b, ab), labels):
-        ns.plot.kde_2d(x, y, ax=ax, label=label, alpha=0.5)
+    for ns, label, alpha in zip((a, b, ab), labels, (0.5, 0.5, 0.9)):
+        ns.plot.kde_2d(x, y, ax=ax, label=label, alpha=alpha)
 
     ax.set(xlabel=a.get_label(x), ylabel=a.get_label(y),
            xlim=(0.18, 0.50), ylim=(0.70, 1.00))
     ax.legend()
     return ax
+
+
+@samples
+def plot_corner(a, b, ab, axes, labels=("a", "b", "ab")):
+    for ns, label, alpha in zip((a, b, ab), labels, (0.5, 0.5, 0.9)):
+        ns.plot_2d(axes, label=label, alpha=alpha)
+    axes.iloc[1, 0].legend(loc=(3, 0.5))
+
+    return axes

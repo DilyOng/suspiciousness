84f1db5019ed865aed31bdeb29c521b84b7b17cb
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-05-09 14:39:47 +0100

    update interface

--- suspiciousness/plot.py
+++ suspiciousness/plot.py
@@ -4,23 +4,28 @@ from suspiciousness.core import samples
 
 
 @samples
-def sigma8plot(a, b, ab, labels=("a", "b", "ab"), ax=None, x="omegam", y="sigma8"):
+def sigma8plot(*nestedsamples, ax=None, x="omegam", y="sigma8", plot_kwargs):
     if ax is None:
         _, ax = plt.subplots()
 
-    for ns, label, alpha in zip((a, b, ab), labels, (0.5, 0.5, 0.9)):
-        ns.plot.kde_2d(x, y, ax=ax, label=label, alpha=alpha)
+    for ns, alpha in zip(nestedsamples, (0.5, 0.5, 0.9)):
+        ns.plot.kde_2d(x, y, ax=ax, alpha=alpha, **plot_kwargs[ns.label])
 
-    ax.set(xlabel=a.get_label(x), ylabel=a.get_label(y),
+    ax.set(xlabel=nestedsamples[0].get_label(x),
+           ylabel=nestedsamples[0].get_label(y),
            xlim=(0.18, 0.50), ylim=(0.70, 1.00))
     ax.legend()
     return ax
 
 
 @samples
-def cornerplot(a, b, ab, axes, labels=("a", "b", "ab")):
-    for ns, label, alpha in zip((a, b, ab), labels, (0.5, 0.5, 0.9)):
-        ns.plot_2d(axes, label=label, alpha=alpha)
+def cornerplot(axes, *nestedsamples, prior=False, plot_kwargs):
+    if prior:
+        nestedsamples[0].set_beta(0).plot_2d(axes, alpha=0.25, color="blue", label="prior")
+
+    for ns, alpha in zip(nestedsamples,
+                                (0.5, 0.5, 0.9)):
+        ns.plot_2d(axes, alpha=alpha, **plot_kwargs[ns.label])
     axes.iloc[1, 0].legend(loc=(3, 0.5))
 
     return axes

945ba3d5987c441940258cd388cd7a2b33bff1fc
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-05-10 23:26:43 +0100

    allow case where plot_kwargs doesn't contain that dataset

--- suspiciousness/plot.py
+++ suspiciousness/plot.py
@@ -9,7 +9,8 @@ def sigma8plot(*nestedsamples, ax=None, x="omegam", y="sigma8", plot_kwargs):
         _, ax = plt.subplots()
 
     for ns in nestedsamples:
-        ns.plot.kde_2d(x, y, ax=ax, **plot_kwargs[ns.label])
+        kwargs = plot_kwargs.pop(ns.label, {})
+        ns.plot.kde_2d(x, y, ax=ax, **kwargs)
 
     ax.set(xlabel=nestedsamples[0].get_label(x),
            ylabel=nestedsamples[0].get_label(y),
@@ -21,10 +22,12 @@ def sigma8plot(*nestedsamples, ax=None, x="omegam", y="sigma8", plot_kwargs):
 @samples
 def cornerplot(axes, *nestedsamples, prior=False, plot_kwargs):
     if prior:
-        nestedsamples[0].set_beta(0).plot_2d(axes, alpha=0.25, color="blue", label="prior")
+        nestedsamples[0].set_beta(0).plot_2d(axes, alpha=0.25,
+                                             color="blue", label="prior")
 
     for ns in nestedsamples:
-        ns.plot_2d(axes, **plot_kwargs[ns.label])
+        kwargs = plot_kwargs.pop(ns.label, {})
+        ns.plot_2d(axes, **kwargs)
     axes.iloc[1, 0].legend(loc=(3, 0.5))
 
     return axes

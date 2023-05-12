7174467e4436c167f2b8f82e9754db51f4a6eb83
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-05-12 16:36:34 +0100

    add p to __init__.py

--- suspiciousness/__init__.py
+++ suspiciousness/__init__.py
@@ -1,3 +1,3 @@
-from suspiciousness.core import logR, logI, logS, logp
+from suspiciousness.core import logR, logI, logS, logp, p
 from suspiciousness.plot import cornerplot, sigma8plot
 from suspiciousness.utils import samples, stats

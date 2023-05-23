3aaba09ec29eb3f986d6cdb81f83e839b7b91f79
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-05-23 12:20:03 +0100

    don't use print as a variable lmao

--- suspiciousness/core.py
+++ suspiciousness/core.py
@@ -3,25 +3,25 @@ from scipy.stats import chi2
 
 
 @stats
-def logR(a, b, ab, print=False):
+def logR(a, b, ab, show=False):
     logR = ab.logZ - a.logZ - b.logZ
-    if print:
+    if show:
         print(f"logR = {logR.mean()} ± {logR.std()}")
     return logR
 
 
 @stats
-def logS(a, b, ab, print=False):
+def logS(a, b, ab, show=False):
     logS = ab.logL_P - a.logL_P - b.logL_P
-    if print:
+    if show:
         print(f"logS = {logS.mean()} ± {logS.std()}")
     return logS
 
 
 @stats
-def logI(a, b, ab, print=False):
+def logI(a, b, ab, show=False):
     logI = a.D_KL + b.D_KL - ab.D_KL
-    if print:
+    if show:
         print(f"logI = {logI.mean()} ± {logI.std()}")
     return logI
 
@@ -32,18 +32,18 @@ def bayesian_d(a, b, ab):
 
 
 @stats
-def logp(a, b, ab, print=False):
+def logp(a, b, ab, show=False):
     d = bayesian_d(a, b, ab)
     logp = chi2.logsf(d-2*logS(a, b, ab), d)
-    if print:
+    if show:
         print(f"logp = {logp.mean()} ± {logp.std()}")
     return logp
 
 
 @stats
-def p(a, b, ab, print=False):
+def p(a, b, ab, show=False):
     d = bayesian_d(a, b, ab)
     p = chi2.sf(d-2*logS(a, b, ab), d)
-    if print:
+    if show:
         print(f"p = {p.mean()} ± {p.std()}")
     return p
